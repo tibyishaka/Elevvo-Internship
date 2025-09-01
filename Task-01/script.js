ab01c030d552af2fc359a4434c03bba57be75fdf
tibyishaka <t.ibyishaka@alustudent.com>
2025-09-01 17:37:34 +0400

    feat:done with task 01

---
+++ Task-01/script.js
@@ -0,0 +1,6 @@
+ const sidebar = document.getElementById('sidebar');
+    const toggleBtn = document.getElementById('toggleBtn');
+
+    toggleBtn.addEventListener('click', () => {
+      sidebar.classList.toggle('closed');
+    });
